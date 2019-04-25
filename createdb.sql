43d3f6a01c6fc90ab32ee73ad95b0328024e3f44
Peter Fry <peterfry@Peters-MacBook-Pro-2.local>
2019-04-25 11:59:47 +0100

    missed db user hardcoding

--- createdb.sql
+++ createdb.sql
@@ -2,7 +2,7 @@
 \set schoolExperienceDB `echo ${DATABASE_NAME}`
 \set dbuser `echo ${dbuser}`
 CREATE DATABASE :schoolExperienceDB;
-CREATE USER railsappuser WITH PASSWORD :'postgresUserPassword';
+CREATE USER :dbuser WITH PASSWORD :'postgresUserPassword';
 GRANT ALL PRIVILEGES ON DATABASE :schoolExperienceDB TO :dbuser;
 \c :schoolExperienceDB
 CREATE EXTENSION IF NOT EXISTS postgis;
